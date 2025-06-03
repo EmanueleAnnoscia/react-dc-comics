e2f0e5f9a1b1d5adc1062cc6384e7dc9f819e12b
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-03 15:02:43 +0200

    feat carosello fumetti in sezione main

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,17 @@
 import styles from "./AppMain.module.css";
+import comics from "../data/comics.js"
 
 function MainContent() {
   return <main className={styles.main}>
+      {<img src="/jumbotron.jpg" alt="teen-titans-go" className={styles.jumboImg}/>}
       <div className={styles.contentBox}>
-        &rarr; Content goes here &larr;
+        <button>serie correnti</button>
+        {comics.map((curComic) =>(
+          <div key={curComic.id} className={styles.comicCard}>
+            <img  src={curComic.thumb} alt={`Immagine del ${curComic.title}`} />
+            <h6>{curComic.series}</h6>
+          </div>
+        ))}
       </div>
     </main>;
 }
