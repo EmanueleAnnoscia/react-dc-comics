7b85ac1f479cbbaa71d8018047ec6ce1179ac70e
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-03 15:59:04 +0200

    feat map on Banner Logo

--- src/components/AppBannerBonus.jsx
+++ src/components/AppBannerBonus.jsx
@@ -1,14 +1,19 @@
 import styles from "./AppBannerBonus.module.css";
+import { bannerLogo } from "../data/comics.js";
 
 function BannerBonus() {
   return (
     <section className={styles.banner}>
       <div className={styles.boxElem}>   {/* si può mettere anche la nomenclatura classica purchè sia nelle bracket notation ["box-elem"]  */}
-      <div className={styles.item}>
-        <img src="/buy-comics-digital-comics.png" alt="Digital Comics" />
-        <span>DIGITAL COMICS</span>
-      </div>
-      <div className={styles.item}>
+     
+        {bannerLogo.map((curLogo)=> (
+           <div className={styles.item}>
+            <img src={curLogo.src} alt={curLogo.alt} />
+            <span>{curLogo.title}</span>
+          </div>
+        ))}
+        
+      {/* <div className={styles.item}>
         <img src="buy-comics-merchandise.png" alt="DC Merchandise" />
         <span>DC MERCHANDISE</span>
       </div>
@@ -23,7 +28,7 @@ function BannerBonus() {
       <div className={styles.item}>
         <img src="buy-dc-power-visa.svg" alt="DC Power Visa" />
         <span>DC POWER VISA</span>
-      </div>
+      </div> */}
       </div>
     </section>
   );
