1742364bd799c5d004de06f040fcdaf9528a160e
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-04 14:36:07 +0200

    feat immagine not found place holder utilizzando short circuit evaluation

--- src/components/ComicsCard.jsx
+++ src/components/ComicsCard.jsx
@@ -5,8 +5,9 @@ import styles from "./AppMain.module.css";
 const ComicsCard = ({ thumb, series, title}) => {
     return (
         <div className={styles.comicCard}>
-            <img  src={thumb} alt={title}  />
+            <img  src={thumb || "https://placehold.co/300x300"} alt={title}  />
             <h6>{series}</h6>
+            
         </div>
     );
 };   
